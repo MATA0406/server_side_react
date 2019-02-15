b62dd3c2db93bf3bcf133c91242956191fcc2cc1
MATA <jinho_46@naver.com>
2019-02-15 18:01:03 +0900

    Movie-App

--- src/App.js
+++ src/App.js
@@ -2,39 +2,105 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie.js';
 
-const movies = [
-  {
-    id : 1,
-    title : "Matrix",
-    poster : "https://ae01.alicdn.com/kf/HTB1RPP8QVXXXXX4aXXXq6xXFXXXP/Keanu.jpg_640x640.jpg"
-  },
-  {
-    id : 2,
-    title : "Full Metal Jacket",
-    poster : "https://dhgywazgeek0d.cloudfront.net/watcha/image/upload/c_fill,h_400,q_80,w_280/v1485423697/bvacgjpzkogqdbcbo506.jpg"
-  },
-  {
-    id : 3,
-    title : "Oldboy",
-    poster : "http://vod.busan.go.kr/movie/poster/poster16.jpg"
-  },
-  {
-    id : 4,
-    title : "Star Wors",
-    poster : "https://upload.wikimedia.org/wikipedia/ko/thumb/6/6a/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg/250px-%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg"
-  }
-]
+
 
 class App extends Component {
+
+  // Life Cycle
+  // Render: componentWillMount() -> render() -> ComponentDidMount()
+  // Update: componentWillReceiveProps() -> shouldComponentUpdate() == true -> componentWillUpdate() -> render() -> component
+
+  state = {
+
+  }
+
+  componentWillMount(){
+    console.log('will mount');
+  }
+  
+  componentDidMount(){
+    console.log('did mount');
+
+    this._getMovies();
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map(movie => {
+      return <Movie
+        title={movie.title_english}
+        poster={movie.medium_cover_image}
+        key={movie.id}
+        genres={movie.genres}
+        synopsis={movie.synopsis}
+      />
+    })
+
+    return movies;
+  }
+
+  // 비동기 function
+  _getMovies = async () => {
+    const movies = await this._callApi(); // await은 성공이 아닌 결과가 끝난 뒤 실행
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+     // AJAX
+     return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+     .then(response => response.json())
+     .then(json => json.data.movies) // 화살표 fucntion은 return을 작성할 필요가 없다.(자동!)
+     .catch(err => console.log(err))
+  }
+
   render() {
+    const { movies } = this.state;
+
     return (
-      <div className="App">
-        {movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index}/>
-        })}
+      <div className={movies ? "App" : "App--loading"}>
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     )
   }
 }
 
 export default App;
+
+// state를 변경하는 방법 setState() - state를 변경하면 render가 발생한다.
+// this.setState({
+//   greeting : 'Hello again!'
+// })
+
+
+// 00 시간 후에 00 작업을 수행시킨다.
+// setTimeout(() => {
+//   this.setState({
+//     movies : [
+//       {
+//         id : 1,
+//         title : "Poket Monster1",
+//         poster : "http://image.yes24.com/goods/2125649/L"
+//       },
+//       {
+//         id : 2,
+//         title : "Poket Monster2",
+//         poster : "http://vignette3.wikia.nocookie.net/pokemon/images/e/e2/2%EA%B8%B0_%EA%B7%B9%EC%9E%A5%ED%8C%90_%ED%8F%AC%EC%8A%A4%ED%84%B0_2.jpg/revision/latest?cb=20150812225936&path-prefix=ko"
+//       },
+//       {
+//         id : 3,
+//         title : "Poket Monster3",
+//         poster : "https://t1.daumcdn.net/cfile/tistory/999C40405C316B5810"
+//       },
+//       {
+//         id : 4,
+//         title : "Poket Monster4",
+//         poster : "https://vignette.wikia.nocookie.net/pokemon/images/9/99/8%EA%B8%B0_%EA%B7%B9%EC%9E%A5%ED%8C%90_%ED%8F%AC%EC%8A%A4%ED%84%B0.jpg/revision/latest?cb=20101022180227&path-prefix=ko"
+//       },
+//       {
+//         title : "Poket Monster5",
+//         poster : "https://lh3.googleusercontent.com/proxy/OIHWkhCQOgzGTY-6u-5ugUi6y1hf4o2Lb-dS4duG_aiXe68-0_Lv4hm8Q3f5T5HI7ldCeIigUeCv9V0A4I5CoeDtqf1x50zOpIcFM4mgzbECMQk=w200-h300"
+//       }
+//     ]
+//   })
+// }, 5000)
--- src/Movie.js
+++ src/Movie.js
@@ -2,36 +2,54 @@ import React, { Component } from 'react';
 import PropType from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component {
-
-    // 부모에게 받는 파라미터의 타입을 알 수 있다.(return true & false)
-    static proptype = {
-        title : PropType.string.isRequired,
-        poster : PropType.string.isRequired
-    }
-
-    render() {
-        return(
-            <div>
-                <h1>{this.props.title}</h1>
-                <MoviePoster poster={this.props.poster}/>
+// Class가 아닌 Dumb function
+function Movie({title, poster, genres, synopsis}){
+    return(
+        <div className="Movie">
+            <div className="Movie_Columns">
+                <MoviePoster poster={poster} alt={title}/>
             </div>
-        )
-    }
+            <div className="Movie_Columns">
+                <h1>{title}</h1>
+                <div className="Movie_Genrs">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+                </div>
+                <p className="Movie_Synopsis">
+                    {synopsis}
+                </p>
+            </div>
+        </div>
+    )
 }
 
-class MoviePoster extends Component {
+// Dumb function
+// Life Cycle도 없고 그냥 function을 리턴할뿐(state도 없다.)
+function MoviePoster({poster, alt}){
+    return(
+        <img alt={alt} src={poster} className="Movie_Poster" />
+    )
+}
 
-    // 부모에게 받는 파라미터의 타입을 알 수 있다.(return true & false)
-    static proptype = {
-        poster : PropType.string.isRequired
-    }
+function MovieGenre({genre}){
+    return(
+        <span className="Movie_Genre">{genre}</span>
+    )
+}
 
-    render() {
-        return(
-            <img alt="testImage" src={this.props.poster} />
-        )
-    }
+Movie.propTypes = {
+    title : PropType.string.isRequired,
+    poster : PropType.string.isRequired,
+    genres : PropType.array.isRequired,
+    synopsis : PropType.string.isRequired
+}
+
+MoviePoster.propTypes = {
+    poster : PropType.string.isRequired,
+    alt : PropType.string.isRequired
+}
+
+MovieGenre.propTypes = {
+    genre : PropType.string.isRequired
 }
 
 export default Movie;
