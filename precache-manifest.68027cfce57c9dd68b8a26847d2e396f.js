c0af0a1ab5dbce8f66b426119f8fb56437e7c127
MATA <jinho_46@naver.com>
2019-02-18 10:30:28 +0900

    Updates

---
+++ precache-manifest.68027cfce57c9dd68b8a26847d2e396f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6b05499f0f597a92805",
+    "url": "/server_side_react/static/js/runtime~main.c6b05499.js"
+  },
+  {
+    "revision": "14afcf83492a9657447c",
+    "url": "/server_side_react/static/js/main.14afcf83.chunk.js"
+  },
+  {
+    "revision": "e2a4fca7c1285f971d4a",
+    "url": "/server_side_react/static/js/2.e2a4fca7.chunk.js"
+  },
+  {
+    "revision": "14afcf83492a9657447c",
+    "url": "/server_side_react/static/css/main.2cb210f9.chunk.css"
+  },
+  {
+    "revision": "9ca2d58e705eebd2e4abe334c3a976a4",
+    "url": "/server_side_react/index.html"
+  }
+];
